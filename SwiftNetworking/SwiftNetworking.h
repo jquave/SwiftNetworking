1d52dc6248d79373ed606e981af510c96ec16817
Jameson Quave <jquave@gmail.com>
2014-06-08 14:48:52 -0500

    Initial Commit

---
+++ SwiftNetworking/SwiftNetworking.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNetworking.h
+//  SwiftNetworking
+//
+//  Created by Jameson Quave on 6/8/14.
+//  Copyright (c) 2014 JQ Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftNetworking.
+FOUNDATION_EXPORT double SwiftNetworkingVersionNumber;
+
+//! Project version string for SwiftNetworking.
+FOUNDATION_EXPORT const unsigned char SwiftNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNetworking/PublicHeader.h>
+
+
